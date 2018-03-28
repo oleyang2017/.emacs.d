d9c02c0169013cb5198b7271ef58c7c8a5f3d77c
oleyang <oleyang@qq.com>
2018-03-28 19:04:28 +0800

    add diary directory

--- custom.el
+++ custom.el
@@ -112,7 +112,9 @@
 (global-set-key [(control tab)] 'kill-this-buffer) ; 关闭当前文件
 
 ;; magit
-(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x g s") 'magit-status)
+(global-set-key (kbd "C-x g u") 'magit-push)
+(global-set-key (kbd "C-x g p") 'magit-pull)
 
 ;; xml
 ;; 编辑xml时C-c C-f 自动完成关闭标签，例如</div>
@@ -183,3 +185,6 @@ For example, searching `foobar' matches `foo-bar' or `f-o-o=b<a>r'."
 ;; isearch+
 (add-to-list 'load-path "~/.emacs.d/elpa/isearch+-20170723.1826")
 (require 'isearch+)
+
+;; diary
+(setq diary-file "~/diary/diary")
